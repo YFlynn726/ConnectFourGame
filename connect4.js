77ae8c2164943f6c95400eaad984010b09bd0fd6
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2021-05-03 21:59:51 -0400

    fixed variable names and endGame fx

--- connect4.js
+++ connect4.js
@@ -16,8 +16,6 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-  //don't understand this logic____________________________
   for (let i = 0; i < HEIGHT; i++) {
     board.push(Array.from({ length: WIDTH }));
   }
@@ -26,25 +24,21 @@ function makeBoard() {
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-
   const htmlBoard = document.querySelector("#board");
 
-  // TODO: add comment for this code
   //this is the heading of the table-clickable space of board
-  const top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
+  const topRow = document.createElement("tr");
+  topRow.setAttribute("id", "row-top");
+  topRow.addEventListener("click", handleClick);
 
   for (let x = 0; x < WIDTH; x++) {
     const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
-    top.append(headCell);
+    topRow.append(headCell);
   }
-  htmlBoard.append(top);
+  htmlBoard.append(topRow);
 
-  // TODO: add comment for this code
-  //playing space of the board
+  //playing space for the board
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (let x = 0; x < WIDTH; x++) {
@@ -58,9 +52,6 @@ function makeHtmlBoard() {
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 function findSpotForCol(x) {
-  //got stuck with this code_________________________________
-
-  // TODO: write the real version of this, rather than always returning 0
   for (let y = HEIGHT - 1; y >= 0; y--) {
     if (!board[y][x]) {
       return y;
@@ -72,22 +63,26 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
   const tableCell = document.getElementById(`${y}-${x}`);
-  const div = document.createElement("div");
-  div.classList.add("piece");
-  div.classList.add(`player${currPlayer}`);
+  const piece = document.createElement("div");
+  piece.classList.add("piece");
+  piece.classList.add(`player${currPlayer}`);
 
-  tableCell.append(div);
+  tableCell.append(piece);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
   alert(msg);
   board = [];
   //how to clear game pieces
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      const tableCell = document.getElementById(`${y}-${x}`);
+      tableCell.innerHTML = "";
+    }
+  }
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -103,7 +98,6 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   board[y][x] = currPlayer;
   placeInTable(y, x);
 
@@ -113,13 +107,11 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
   if (board.every((row) => row.every((cell) => cell))) {
     return endGame("Tie! Play again!");
   }
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
   if (currPlayer !== 1) {
     return (currPlayer = 1);
   } else {
@@ -145,29 +137,28 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
   //checking(looping) each cell for pieces dropped and if there is four in row(horizontally, vertically, or diagonally)
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      let horiz = [
         [y, x],
         [y, x + 1],
         [y, x + 2],
         [y, x + 3],
       ];
-      var vert = [
+      let vert = [
         [y, x],
         [y + 1, x],
         [y + 2, x],
         [y + 3, x],
       ];
-      var diagDR = [
+      let diagDR = [
         [y, x],
         [y + 1, x + 1],
         [y + 2, x + 2],
         [y + 3, x + 3],
       ];
-      var diagDL = [
+      let diagDL = [
         [y, x],
         [y + 1, x - 1],
         [y + 2, x - 2],
